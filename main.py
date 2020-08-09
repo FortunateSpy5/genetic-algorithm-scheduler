b0b0a55efeaa325bbf734ef42a742b2a88143b5e
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-08-09 18:11:08 +0530

    Update main.py
    Class Info completed

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
 import random
 
 
+class Info:
+    def __init__(self, data=None):
+        default = {
+            "institute": "RCC Institute of Information Technology",
+            "department_count": 1,
+            "departments": {
+                "CSE": {
+                    "section_count": 2,
+                    "sections": ["5A, 5B"],
+                    "courses": {
+                        "ESC-501": {
+                            "name": "Software Engineering",
+                            "teacher": ["Mrs. Monica Singh"],
+                            "class_count": 3
+                        },
+                        "PCC-CS-501": {
+                            "name": "Compiler Design",
+                            "teacher": ["Dr. Anup Kumar Kolya"],
+                            "class_count": 3
+                        },
+                        "PCC-CS-502": {
+                            "name": "Operating System",
+                            "teacher": ["Mr. Harinandan Tunga"],
+                            "class_count": 3
+                        },
+                        "PCC-CS-503": {
+                            "name": "Object Oriented Programming",
+                            "teacher": ["Mr. Arup Kumar Bhattacharjee"],
+                            "class_count": 3
+                        },
+                        "PEC-IT-501": {
+                            "name": "Theory of Computation / Computer Graphics",
+                            "teacher": ["Mr. Rajib Saha", "Sk. Mazharul Islam"],
+                            "class_count": 3
+                        },
+                        "MC-CS-501": {
+                            "name": "Constitution of India",
+                            "teacher": ["Dr. Sadhan Kumar Dey"],
+                            "class_count": 2
+                        },
+                        "HS-MC-501": {
+                            "name": "Introduction to Industrial Management",
+                            "teacher": ["Mrs. Jhuma Ray"],
+                            "class_count": 2
+                        }
+                    }
+                }
+            }
+        }
+        if data is None:
+            self.data = default
+        else:
+            self.data = data
+
+
 class Schedule:
     def __init__(self, days=5, slots_per_day=4):
         self.days = days
@@ -8,52 +63,10 @@ class Schedule:
         self.total_slots = self.slots_per_day * self.days
         self.time_per_class = None
         self.departments = []
-        self.timetable = []
+        self.department_count = 0
 
     def create_departments(self):
-        self.departments.append(
-            Department(
-                "CSE",
-                [
-                    Course(
-                        "PCCCS501",
-                        ["Kolya"],
-                        3
-                    ),
-                    Course(
-                        "PCCCS502",
-                        ["Tunga"],
-                        3
-                    ),
-                    Course(
-                        "PCCCS503",
-                        ["Bhattachajee"],
-                        3
-                    ),
-                    Course(
-                        "ESC501",
-                        ["Monica"],
-                        3
-                    ),
-                    Course(
-                        "PECIT501",
-                        ["Saha"],
-                        3
-                    ),
-                    Course(
-                        "MCCS501",
-                        "Dey",
-                        2
-                    ),
-                    Course(
-                        "MCCS502",
-                        ["Ray"],
-                        2
-                    )
-                ],
-                ["A", "B"]
-            )
-        )
+        pass
 
     def generate_schedule(self):
         for department in range(len(self.departments)):
@@ -84,15 +97,20 @@ class Schedule:
 
 
 class Department:
-    def __init__(self, name, courses, sections):
+    def __init__(self, name):
         self.name = name
-        self.courses = courses
-        self.sections = sections
+        self.courses = []
+        self.sections = []
+        self.section_count = None
 
+class Section:
+    def __init__(self, name):
+        self.name = name
 
 class Course:
-    def __init__(self, name, teachers, class_count):
+    def __init__(self, name, code, teachers, class_count):
         self.name = name
+        self.code = code
         self.teacher = teachers
         self.class_count = class_count
 
@@ -107,11 +125,6 @@ class Class:
 class Teacher:
     def __init__(self, name):
         self.name = name
-
-
-class Info:
-    def __init__(self):
-        self.data = {}
 
 
 class Population:
